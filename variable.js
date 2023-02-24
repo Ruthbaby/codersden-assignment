0f7287e5b4cc52ba8c71bb13123a28456a3dae1a
Ruth Iroanusi <iroanusi.ruth@gmail.com>
2023-02-24 22:04:49 +0100

    Day2 Assignment

--- variable.js
+++ variable.js
@@ -10,3 +10,22 @@ var num1 = true ;
 var num2;
 var num3 = null;
 
+let challenge = "30 Days of JavaScript"
+
+console.log(challenge);
+// to check the length string
+console.log(challenge.length);
+
+// changing words to uppercase
+console.log(challenge.toUpperCase());
+
+// changing words toLowerCase
+console.log(challenge.toLowerCase());
+
+// slicing the the first word with string
+// Cut (slice) out the first word of the string using substr() or substring() method
+// Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+// Check if the string contains a word Script using includes() method
+
+let myLearning = "JavaScript";
+console.log(myLearning.substr(1, 9));
