4fb0197a7285c6dbb3d527f02e2f163f3db96707
Ruth Iroanusi <iroanusi.ruth@gmail.com>
2023-02-24 15:45:43 +0100

    Day one Assignment

---
+++ datatype.js
@@ -0,0 +1,46 @@
+// Assignment number 4
+
+var myName = 'Ruth';
+var num = 30;
+var num1 = true;
+var num2;
+var num3 = null;
+
+console.log(typeof 'Ruth'); // string
+console.log(typeof 30); // number
+console.log(typeof true); // boolean
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object type
+
+//Variables without assigning values
+
+let city;
+let country;
+let town;
+let location;
+//variables with assigned values
+
+let Learning = 'JavaScript';
+let code = 'Coders Den';
+let state = 'Lagos';
+let noName = 23;
+
+/*Declare variables to store your first name, last name, marital status, country and age in multiple lines*/
+
+let firstName = 'Ruth';
+let lastName = 'Iroanusi';
+let status = 'Married';
+let country1 = 'Nigeria';
+let age1 = 40;
+
+// Single line declaration of variables
+let name = 'Kingsley',
+  occupation = 'Student',
+  education = 'graduate',
+  age = 90;
+
+let myAge = 45;
+let yourAge = 23;
+
+console.log('I am ' + myAge + '' + 'years old');
+console.log('You are ' + yourAge + '' + 'years old');
---
+++ variable.js
@@ -0,0 +1,12 @@
+//Comments make code readable.
+//Welcome to 30DaysOfJavaScript
+/* comments can make code readable, easy to reuse and informative*/
+
+/*Create a variable.js file and declare variables and assign string, boolean, undefined and null data types.*/
+
+var myName = "Ruth";
+var num = 30;
+var num1 = true ;
+var num2;
+var num3 = null;
+
